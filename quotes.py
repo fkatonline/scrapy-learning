d6a4fb18c467aacc8ffa72ba83e36ec80a25e397
Fkat <fkatonline@gmail.com>
2020-01-27 21:30:49 +0700

    3nd

--- quotes.py
+++ quotes.py
@@ -8,9 +8,15 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
-        self.log("I just visited here.")
-        yield {
-            'author': response.css('small.author::text').extract_first(),
-            'text': response.css('span.text::text').extract_first(),
-            'tags': response.css('a.tag::text').extract(),
-        }
+        for quote in response.css('div.quote'):
+            item = {
+                'author': response.css('small.author::text').extract_first(),
+                'text': response.css('span.text::text').extract_first(),
+                'tags': response.css('a.tag::text').extract(),
+            }
+            yield item
+        # follow the next page
+        next_page_url = response.css('li.next a::attr(href)').get()
+        if next_page_url is not None:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(next_page_url, callback=self.parse)
