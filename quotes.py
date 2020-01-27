e15143469b7c72301f3e63b1e25f3a63e156b284
Fkat <fkatonline@gmail.com>
2020-01-27 19:42:33 +0700

    2nd

--- quotes.py
+++ quotes.py
@@ -5,7 +5,12 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['toscrape.com']
-    start_urls = ['http://toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
         self.log("I just visited here.")
+        yield {
+            'author': response.css('small.author::text').extract_first(),
+            'text': response.css('span.text::text').extract_first(),
+            'tags': response.css('a.tag::text').extract(),
+        }
